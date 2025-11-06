53d0baa93d063a4b1f8de3172f78f908da0659ae
mujikajulenuni1801 <mujikajulenuni@gmail.com>
2025-11-06 10:14:38 +0100

    Todo en inglés

--- carga_datos.py
+++ carga_datos.py
@@ -1,5 +1,5 @@
 # Librerías
-import pandas as pd
+import pandas as pd 
 import numpy as np
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
@@ -7,41 +7,53 @@ import matplotlib.pyplot as plt
 # Archivos de datos
 import columnas
 
+#########################
+### PARTICULARIZACIÓN ###
+#########################
+
 index = columnas.KneeR["idx"]  # Índice de la columna con la que se trabaja
 mensaje = columnas.KneeR["msg"] # Eje Y en el gráfico 
+archive_name = "resultados_Joseba_cerca.csv"  # Nombre del archivo CSV a cargar
+
+#########################
+#### CARGA DE DATOS #####
+#########################
 
 # Dirección en la que se almacenan los datos procesados en formato CSV
-direccion_csv = "/home/mujikajulen/Documentos/TFM/DatosProcesados/"
+path_csv = "/home/mujikajulen/Documentos/TFM/DatosProcesados/"
 
 # Cargar el CSV con separador y formato decimal UTF-8
-df = pd.read_csv(direccion_csv + "resultados_Joseba_cerca.csv", sep=";", decimal=",")
+df = pd.read_csv(path_csv + archive_name, sep = ";", decimal = ",")
 df.columns = df.columns.str.strip() # Eliminar espacios en los nombres de columnas
 
 # Verifica columnas
-print("Columnas detectadas:", df.columns.tolist())
+# print("Columnas detectadas:", df.columns.tolist())
+
+###########################
+### EXTRACCIÓN DE DATOS ###
+###########################
 
 # Extraer la serie de ángulos, seleccionando los datos por índice
 angles = df.iloc[:, index]  # Ángulo de la rodilla izquierda
 
 # Detectar mínimos en la serie de ángulos
-minimos, _ = find_peaks(-angles, height=-150, distance=10)
+minimums, peak_values = find_peaks(-angles, height=-150, distance=10)
 
 # minimos: devuelve na lista de índices donde se detectan los mínimos
-# _ : diccionario con el valor de cada pico detectado
+# peak_values : diccionario con el valor de cada pico detectado
 
 
 # Eliminar datos inválidos
-valores_minimos = angles.iloc[minimos]
-umbral_minimos = np.mean(valores_minimos) - 10
-minimos_filtrados = minimos[(valores_minimos > (np.mean(valores_minimos) - 10)) & 
-                            (valores_minimos < (np.mean(valores_minimos) + 10))]
+minimum_values = angles.iloc[minimums]
+filtered_minimum = minimums[(minimum_values > (np.mean(minimum_values) - 10)) & 
+                            (minimum_values < (np.mean(minimum_values) + 10))]
 
 # Mostrar resultados
-print("Frames con mínimos detectados:", minimos_filtrados)
+print("Frames con mínimos detectados:", filtered_minimum)
 
 # Visualizar
 plt.plot(angles)
-plt.plot(minimos_filtrados, angles[minimos_filtrados], "rx")
+plt.plot(filtered_minimum, angles[filtered_minimum], "rx")
 plt.title("Detección de ciclos en la marcha")
 plt.xlabel("Frame")
 plt.ylabel(mensaje)
