d34f3d051e34506a4d6c06494943ac0f7de38230
mujikajulenuni1801 <mujikajulenuni@gmail.com>
2025-11-13 12:47:45 +0100

    VIsualización Correcta

--- carga_datos.py
+++ carga_datos.py
@@ -21,18 +21,20 @@ def data_csv(file_path, file_name):
     return df
 
 def minimum_extraction(angles, height, distance):
-    minimums, peak_values = find_peaks(-angles, -height, -distance)
+    minimums, peak_values = find_peaks(-angles, height = -height, distance = distance)
     minimum_values = angles.iloc[minimums]
     filtered_minimum = minimums[(minimum_values > (np.mean(minimum_values) - 10)) & 
                             (minimum_values < (np.mean(minimum_values) + 10))]
     return filtered_minimum
 
 def visualization(angles, filtered_minimum, title):
+    plt.figure(figsize=(12,6))
     plt.plot(angles) 
     plt.plot(filtered_minimum, angles[filtered_minimum], "rx")
     plt.title(title)
     plt.xlabel("Frame")
     plt.ylabel("Grados")
+    plt.grid(True)
     plt.show()
     
 def gait_iteration(filtered_minimum, angles):
@@ -42,15 +44,14 @@ def gait_iteration(filtered_minimum, angles):
         end_frame = filtered_minimum[i+1]
         gait_cycle = angles.loc[init_frame:end_frame]
         print(f"Ciclo de la marcha {i+1}. Comienzo: {init_frame}, Fin: {end_frame}")
-        plt.plot(gait_cycle)
-
+        visualization(gait_cycle, [], f"Ciclo de la marcha {i+1}")
 if __name__ == "__main__":
     
     file_name = "resultados_01_01_02.csv"
     file_path = "/home/mujikajulen/Documentos/TFM/DatosProcesados/"
     
-    idx = columnas.AnkleL["idx"]  # Índice de la columna con la que se trabaja
-    msg = columnas.AnkleL["msg"] # Eje Y en el gráfico 
+    idx = columnas.AnkleR["idx"]  # Índice de la columna con la que se trabaja
+    msg = columnas.AnkleR["msg"] # Eje Y en el gráfico 
     
     df = data_csv(file_path, file_name)
     angles = df.iloc[:,idx]
